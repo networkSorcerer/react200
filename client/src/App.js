88c2a6708e69ebf61312e1bfff5630a92a10808e
networkSocerer <gigattfyhig@gmail.com>
2024-06-25 16:10:29 +0900

    react

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,19 @@ import Es6 from './R009_Es6';
 import Variable from './R010_Variable';
 import SpreadOperator from './R011_SpreadOperator';
 import ClassPrototype from './R012_Class&Prototype';
-import ArrowFunc from './R013_ArrowFunction';
+//import ArrowFunc from './R013_ArrowFunction';
 import ForEach from './R014_ForEach';
 import Map from './R015_Map';
+import Jquery from './R016_Jquery';
+import Props from './R017_Props';
+import PropsDatatype from './R018_PropsDatatype.js';
+import PropsBoolean from './R019_PropsBoolean';
+import PropsObjVal from './R20_PropsObjVal';
+import PropsRequired from './R021_PropsRequired';
+import PropsDefault from './R022_PropsDefault';
+import PropsNode from './R023_PropsNode.js';
+import ReactState from './R024_ReactState.js';
+import SetState from './R025_SetState.js';
 function App() {
   return (
     <div className="App">
@@ -31,9 +41,29 @@ function App() {
       <Variable/>
       <SpreadOperator/>
       <ClassPrototype></ClassPrototype>
-      <ArrowFunc/>
+      {/* <ArrowFunc/> */}
       <ForEach/>
       <Map/>
+      <Jquery/>
+      <Props props_val="THIS IS PROPS"/>
+      <PropsDatatype
+      String="react"
+      Number={200}
+      Boolean={1==1}
+      Array={[0,1,8]}
+      Object_Json={{react:"리액트", twohundred:"200"}}
+      Function={console.log("FunctionProps: function!")}
+      />
+      <PropsBoolean BooleanTrueFalse={false}/>
+      <PropsBoolean BooleanTrueFalse/>
+      <PropsObjVal Object_Json={{react:"리액트",twohundred:"200"}}/>
+      <PropsRequired ReactNumber={200}/>
+      <PropsDefault ReactNumber={200}/>
+      <PropsNode>
+        <span>node from App.js</span>
+      </PropsNode>
+      <ReactState reactString={"react"}/>
+      <SetState/>
     </div>
   );
 }
--- client/src/R013_ArrowFunction.js
+++ client/src/R013_ArrowFunction.js
@@ -5,7 +5,7 @@ class R013_ArrowFunction extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arrowFuc : 'React200',
+            arrowFunc : 'React200',
             num: 3
         };
     }
@@ -24,27 +24,27 @@ class R013_ArrowFunction extends Component {
 
     Function2 = (num1, num2) =>{
         let num3 = num1 + num2;
-        console.log(num3+'.Arrow Function : ' + this.state.arrowFuc);
+        console.log(num3+'.Arrow Function : ' + this.state.arrowFunc);
     }
 
     Function3() {
         var this_bind = this;
         setTimeout(function(){
             console.log(this_bind.state.num+'.Ec5 Callback Funtion noBind : ');
-            console.log(this.state.arrowFuc);
+            console.log(this.state.arrowFunc);
         }, 100);
     }
 
     Function4() {
         setTimeout(function(){
-            console.log('4. Es5 Callback Function Bind : '+this.state.arrowFuc);
+            console.log('4. Es5 Callback Function Bind : '+this.state.arrowFunc);
         }.bind(this), 100);
     }
 
     Function5 = (num1, num2, num3) =>{
         const num4 = num1 + num2 + num3;
         setTimeout(()=> {
-            console.log(num4+'.Arrow CallBack Function : '+this.state.arrowFuc);
+            console.log(num4+'.Arrow CallBack Function : '+this.state.arrowFunc);
         }, 100);
     }
 

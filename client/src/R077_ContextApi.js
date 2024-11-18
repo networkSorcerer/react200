0dcb17a2007a0b508b8eec5bcf68e19d16fb38a0
networkSorcerer <gigattfyhig@gmail.com>
2024-11-18 15:10:11 +0900

    props

--- client/src/R077_ContextApi.js
+++ client/src/R077_ContextApi.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Children from "./contextChildren";
+import ContextChild from "./contextChild"; // 파일명 확인 후 정확히 수정
 
 const { Provider, Consumer } = React.createContext();
 export { Consumer };
@@ -7,22 +7,28 @@ export { Consumer };
 class R077_ContextApi extends React.Component {
   constructor(props) {
     super(props);
+    // 초기 상태 정의
+    this.state = {
+      name: "defaultName", // 초기 값 설정
+    };
     this.setStateFunc = this.setStateFunc.bind(this);
   }
 
   setStateFunc(value) {
     this.setState({ name: value });
   }
+
   render() {
     const content = {
-      ...this.state,
+      ...this.state, // this.state가 undefined가 아니도록 초기화된 상태를 사용
       setStateFunc: this.setStateFunc,
     };
     return (
       <Provider value={content}>
-        <Children></Children>
+        <ContextChild /> {/* 닫는 태그 수정 */}
       </Provider>
     );
   }
 }
+
 export default R077_ContextApi;
